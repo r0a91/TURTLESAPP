dc71e865a7ca0a5cb0adb66acdb05283f972b76b
BixclowLaptopWindows <r0alvarez91@gmail.com>
2019-02-25 17:14:12 -0500

    Agregados videos y bloqueo de botones

--- Assets/Scripts/mainManager.cs
+++ Assets/Scripts/mainManager.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mainManager : MonoBehaviour {
+public class mainManager : MonoBehaviour
+{
 
-	public GameObject popup;
-	public GameObject t1Audio, bgMusic;
-	//asd
-	// Use this for initialization
-	void Start () {
-		//popup = GameObject.Find("PopUpT1");
-		//t1Audio = GameObject.Find("Tortuga1Audio");
-		bgMusic = GameObject.Find("BackgroundMusic");
-	}
-	
-	public void cerrarPopUP(){
-		popup.transform.localScale=Vector3.zero;
-		
-		t1Audio.GetComponent<AudioSource>().Stop();
-		bgMusic.GetComponent<AudioSource>().Play();
-		
-	}
+    public GameObject popup;
+    public GameObject t1Audio, bgMusic;
+    //asd
+    // Use this for initialization
+    void Start()
+    {
+        //popup = GameObject.Find("PopUpT1");
+        //t1Audio = GameObject.Find("Tortuga1Audio");
+        bgMusic = GameObject.Find("BackgroundMusic");
+    }
+
+    public void cerrarPopUP()
+    {
+        popup.transform.localScale = Vector3.zero;
+
+        t1Audio.GetComponent<AudioSource>().Stop();
+
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("Tortuga"))
+        {
+            item.GetComponent<CircleCollider2D>().enabled = true;
+
+        }
+        bgMusic.GetComponent<AudioSource>().Play();
+
+    }
 
 }
---
+++ Assets/Scripts/mainManager2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class mainManager2 : MonoBehaviour
+{
+
+    public GameObject popup;
+    public GameObject vPlayer, bgMusic;
+    //asd
+    // Use this for initialization
+    void Start()
+    {
+        //popup = GameObject.Find("PopUpT1");
+        //t1Audio = GameObject.Find("Tortuga1Audio");
+        bgMusic = GameObject.Find("BackgroundMusic");
+    }
+
+    public void cerrarPopUP()
+    {
+        popup.transform.localScale = Vector3.zero;
+
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag("Tortuga"))
+        {
+            item.GetComponent<CircleCollider2D>().enabled = true;
+
+        }
+        vPlayer.GetComponent<VideoPlayer>().Stop();
+        //t1Audio.GetComponent<AudioSource>().Stop();
+        bgMusic.GetComponent<AudioSource>().Play();
+
+    }
+
+}
